3d1a01abcb55887a60bf30ecd7a9f09f404d76bf
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-02-25 19:36:43 -0300

    feat: added logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
-import { UserListComponent } from './shared/components/user-list/user-list.component';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent, NotFoundPageComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
