82e28c816171532832600e54fa811dfdf66b3cf5
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-02-25 19:38:12 -0300

    feat: stylized user list and added remove method in storage service

--- src/app/shared/components/user-list/user-list.component.ts
+++ src/app/shared/components/user-list/user-list.component.ts
@@ -1,11 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { UserResponse } from '../../interfaces/user-response.interface';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.sass',
+  styleUrl: './user-list.component.scss',
 })
 export class UserListComponent {
   @Input() userList: UserResponse[] = [];
+  @Output() removeItem = new EventEmitter<number>();
+
+  removeElement(id: number) {
+    this.removeItem.emit(id);
+  }
 }
