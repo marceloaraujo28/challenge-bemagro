651237fb158ce7a8c03d742398480281b6d3af5a
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2024-02-24 22:50:36 -0300

    feat: search user and rendering persist list

--- src/app/pages/users-page/users-page.component.ts
+++ src/app/pages/users-page/users-page.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserResponse } from '../../shared/interfaces/user-response.interface';
+import { UserService } from '../../shared/services/user.service';
+import { StorageService } from '../../shared/services/storage.service';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-users-page',
   templateUrl: './users-page.component.html',
-  styleUrl: './users-page.component.sass'
+  styleUrl: './users-page.component.sass',
 })
 export class UsersPageComponent {
+  resourceForm: FormGroup;
 
+  notfound: boolean = false;
+  notfoundMessage: string = '';
+  userList: UserResponse[] = [];
+
+  constructor(
+    private _userService: UserService,
+    private _storageService: StorageService,
+    private _formBuilder: FormBuilder
+  ) {
+    this.resourceForm = this._formBuilder.group({
+      search: ['', Validators.compose([Validators.required])],
+    });
+
+    this.userList = this._storageService.getList();
+  }
+
+  ngOnInit(): void {
+    this.resourceForm.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((inputs) => {
+        if (!inputs.search) {
+          this.notfound = false;
+          return;
+        }
+
+        this._userService.getUserByName(inputs.search).subscribe({
+          next: (response) => {
+            this.notfound = false;
+
+            if (response.login) {
+              if (this._storageService.add(response)) {
+                this.userList.push(response);
+              }
+            }
+          },
+          error: () => {
+            this.notfound = true;
+            this.notfoundMessage = 'Resultado não encontrado';
+          },
+        });
+      });
+  }
+
+  clear() {
+    this.resourceForm.reset();
+  }
 }
--- src/app/shared/components/user-list/user-list.component.ts
+++ src/app/shared/components/user-list/user-list.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UserResponse } from '../../interfaces/user-response.interface';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.sass'
+  styleUrl: './user-list.component.sass',
 })
 export class UserListComponent {
-
+  @Input() userList: UserResponse[] = [];
 }
